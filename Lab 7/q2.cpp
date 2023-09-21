920b68be1c384c8b3eeb483b0aba9c03e9e27889
Nishant <92144362+Nishant-28@users.noreply.github.com>
2023-09-21 13:10:20 +0530

    Create q2.cpp

---
+++ Lab 7/q2.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+string findLCS(string str1, string str2) {
+    int m = str1.length();
+    int n = str2.length();
+
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+    // Build the LCS matrix using dynamic programming
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (str1[i - 1] == str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    // Reconstruct the LCS from the matrix
+    int len = dp[m][n];
+    string lcs(len, ' ');
+
+    int i = m, j = n;
+    while (i > 0 && j > 0) {
+        if (str1[i - 1] == str2[j - 1]) {
+            lcs[--len] = str1[i - 1];
+            i--;
+            j--;
+        } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            i--;
+        } else {
+            j--;
+        }
+    }
+
+    return lcs;
+}
+
+int main() {
+    string str1, str2;
+
+    cout << "Enter the first string: ";
+    cin >> str1;
+
+    cout << "Enter the second string: ";
+    cin >> str2;
+
+    string lcs = findLCS(str1, str2);
+    int lcsLength = lcs.length();
+
+    cout << "LCS: " << lcs << endl;
+    cout << "LCS Length: " << lcsLength << endl;
+
+    return 0;
+}
